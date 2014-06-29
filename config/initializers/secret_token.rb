00afe2e436a613fd3de8fac4e3b547f983a8d47e
Trece <treathen@hotmail.com>
2014-06-29 20:13:59 +0800

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xuetang::Application.config.secret_key_base = 'afaf068bf2365fcea0fe5ff621ac27603af1105f7d6c5a44c09e2443cca5f68afa7a5112e36ce19db8ab3f4a8674c3336eb3cbaccb764bce19600e246a159b61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xuetang::Application.config.session_store :cookie_store, key: '_Xuetang_session'
