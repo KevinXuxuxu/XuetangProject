5abf59f485a947ee0874ef8e1550fc3bde6c6c10
JimmyZJX <604590822@qq.com>
2014-07-09 14:25:13 +0800

    modifiction on application_controller: refresh exp_time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def exp_time
+    return 5 * 60
+  end
+
   def currentSID
     if (session[:exp_time] > Time.now)
+      session[:exp_time] = Time.now + exp_time
       return session[:sid]
     end
     return nil
@@ -12,6 +17,7 @@ class ApplicationController < ActionController::Base
 
   def currentUser
     if (session[:exp_time] > Time.now)
+      session[:exp_time] = Time.now + exp_time
       return User.find_by_stu_id(session[:sid])
     end
     return nil
